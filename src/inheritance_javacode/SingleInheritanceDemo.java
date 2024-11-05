011159101a14f44ca193afcc48c462fe404e99b8
KIRUTHIGA <KIRUTHIGA@DESKTOP-E3UBHS1>
2024-11-05 19:10:05 +0530

    inheritance javacode

---
+++ src/inheritance_javacode/SingleInheritanceDemo.java
@@ -0,0 +1,9 @@
+package inheritance_javacode;
+
+public class SingleInheritanceDemo {
+
+	public static void main(String[] args) {
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
+				System.out.println(student);
+				}
+}
